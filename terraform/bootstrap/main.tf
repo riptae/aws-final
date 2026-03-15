fd52b3a9d8510d55e769ce5403f2fb2402acac5e
yun <riptaggart@naver.com>
2026-03-15 17:33:33 +0900

    260315 bootstrapping

---
+++ terraform/bootstrap/main.tf
@@ -0,0 +1,36 @@
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "${var.project_name}-tfstate-bucket-${random_id.suffix.hex}"
+}
+
+resource "aws_s3_bucket_versioning" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "${var.project_name}-tf-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
